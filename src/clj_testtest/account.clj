1dc07f24c1f4050ba56d734a5ce04814d48f159e
Carsten <carsten.ringe@web.de>
2011-03-02 05:35:00 +0100

    Added simple >0 check to withdraw.

--- src/clj_testtest/account.clj
+++ src/clj_testtest/account.clj
@@ -3,7 +3,7 @@
 (defstruct account :amount :owner)
 
 (defn withdraw
-  ([account amount] (- (account :amount) amount)))
+  ([account amount] (if (> (account :amount) amount) (- (account :amount) amount) (account :amount))))
 
 (defn deposit
   ([account amount] (+ (account :amount) amount)))
--- test/clj_testtest/test/account.clj
+++ test/clj_testtest/test/account.clj
@@ -6,10 +6,10 @@
   :amount 1000
   :owner "Fred"))
 (deftest test-withdraw
-  (is (= 900 (withdraw exampleAccount 100))))
+  (is (= 900 (withdraw exampleAccount 100)))
+  (is (= 1000 (withdraw exampleAccount 1001)))
+  (is (= 1 (withdraw exampleAccount 999))))
 
-(def exampleAccount (struct-map account
-  :amount 1000
-  :owner "Fred"))
+
 (deftest test-deposit
   (is (= 1100 (deposit exampleAccount 100))))
